29f3cabc1d3927338d12342eb522bee19fe8d677
Abdelrahman Hamza <64665314+abdelrahmanhz@users.noreply.github.com>
2022-08-09 10:01:30 +0200

    Initial commit

---
+++ deli_meals/lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:deli_meals/screens/category_meals_screen.dart';
+import 'package:deli_meals/screens/meal_details_screen.dart';
+import 'package:flutter/material.dart';
+import 'screens/categories_screen.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DeliMeals',
+      theme: ThemeData(
+          primarySwatch: Colors.pink,
+          accentColor: Colors.amber,
+          canvasColor: const Color.fromRGBO(255, 254, 229, 1),
+          fontFamily: 'Raleway',
+          textTheme: ThemeData.light().textTheme.copyWith(
+                bodyMedium: const TextStyle(
+                  color: Color.fromRGBO(20, 51, 51, 1),
+                ),
+                bodySmall: const TextStyle(
+                  color: Color.fromRGBO(20, 51, 51, 1),
+                ),
+                titleMedium:
+                    const TextStyle(fontSize: 20, fontFamily: 'RobotoCondensed', fontWeight: FontWeight.bold),
+              )),
+      //home: const CategoriesScreen(),
+      routes: {
+        '/': (ctx) => const CategoriesScreen(),
+        CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
+        MealDetailsScreen.routeName: (ctx) => MealDetailsScreen(),
+      },
+      onGenerateRoute: (settings) {
+        print(settings.arguments);
+        // return MaterialPageRoute(builder: (ctx) =>CategoriesScreen());
+      },
+      onUnknownRoute: (settings){
+        return MaterialPageRoute(builder: (ctx) => CategoriesScreen());
+      },
+    );
+  }
+}
