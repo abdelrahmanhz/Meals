b48fde4ff739bb6c0693d193c069e1716a04a2fc
Abdelrahman Hamza <64665314+abdelrahmanhz@users.noreply.github.com>
2022-08-09 11:57:02 +0200

    Add Tabs Screen

--- deli_meals/lib/main.dart
+++ deli_meals/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:deli_meals/screens/category_meals_screen.dart';
 import 'package:deli_meals/screens/meal_details_screen.dart';
+import 'package:deli_meals/screens/tabs_screen.dart';
 import 'package:flutter/material.dart';
 import 'screens/categories_screen.dart';
 
@@ -29,7 +30,7 @@ class MyApp extends StatelessWidget {
               )),
       //home: const CategoriesScreen(),
       routes: {
-        '/': (ctx) => const CategoriesScreen(),
+        '/': (ctx) => const TabsScreen(),
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
         MealDetailsScreen.routeName: (ctx) => MealDetailsScreen(),
       },
